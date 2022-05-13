7d9a48ea162268ca9f2e74c66c55c1c205d044f7
ax-shay <akshay.p.shembekar@live.com>
2022-05-13 00:35:33 -0400

    5/13:APS - Bubble Sort -- Code Commit

--- Sorting/bubble_sort.py
+++ Sorting/bubble_sort.py
@@ -1,34 +1,40 @@
 """
-Time Complexity = O(n^2)
-Space Complexity = O(1)
+input = [3, 8,  9, 4, 7, 1]
+output = [1, 3, 4, 7, 8, 9]
+
+Pass-1:
+1. if 1 < 7, Swap (input[n], input[n-1] = input[n-1], input[n]) [3, 8, 9, 4, 1, 7]
+2. if 1 < 4, Swap (input[n-1], input[n-2] = input[n-2], input[n-1]) [3, 8, 9, 1, 4, 7]
+3. if 1 < 9, Swap (input[n-2], input[n-3] = input[n-3, input[n-2]) [3, 8, 1, 9, 4, 7]
+4. if 1 < 8, Swap (input[n-3], input[n-4] = input[n-4, input[n-3]) [3, 1, 8, 9, 4, 7]
+5. if 1 < 3, Swap (input[n-4], input[n-5] = input[n-5, input[n-4]) [1, 3, 8, 9, 4, 7]
+
+Pass-2:
+1. if 7 < 4, [1, 3, 8, 9, 4, 7]
+2. if 4 < 9, Swap (input[n-1], input[n-2] = input[n-2], input[n-1]) [1, 3, 8, 4, 9, 7]
+3. if 4 < 8, Swap (input[n-2], input[n-3] = input[n-3, input[n-2]) [1, 3, 4, 8, 9, 7]
+4. if 4 < 3, [1, 3, 4, 8, 9, 7]
+
+..
 """
 
 
 class BubbleSort:
-    def __init__(self):
-        self.input_array = []
+    def __init__(self, input):
+        self.input = input
 
-    def _sort(self, ind):
-        self.input_array[ind], self.input_array[ind+1] = self.input_array[ind+1], self.input_array[ind]
+    def _swap(self, j):
+        self.input[j], self.input[j-1] = self.input[j-1], self.input[j]
 
     def bubble_sort(self):
-        has_swapped = True
-        iterator = 0
-
-        while has_swapped:
-            has_swapped = False
-
-            for i in range(len(self.input_array) - iterator - 1):
-                if self.input_array[i] > self.input_array[i+1]:
-                    self._sort(i)
-                    has_swapped = True
-            iterator += 1
-            print("Array at the end of {0} pass is: {1}".format(iterator, self.input_array))
-        return self.input_array
+        for i in range(0, len(self.input)-1):
+            for j in range(len(self.input)-1, i, -1):
+                if self.input[j] < self.input[j-1]:
+                    self._swap(j)
+        return self.input
 
 
-a = BubbleSort()
-a.input_array = [14, 46, 43, 27, 57, 50, 41, 45, 21, 7]
-sorted_array = a.bubble_sort()
-print("Finally, Sorted Array is: " + str(sorted_array))
-
+if __name__ == '__main__':
+    input_list = [3, 8,  9, 4, 7, 1]
+    bubble = BubbleSort(input_list)
+    print(bubble.bubble_sort())
